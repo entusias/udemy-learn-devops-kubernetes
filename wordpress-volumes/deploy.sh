23b50f8bb52bab9d66d1811c228abd51282bff41
Nico S <nocen@posteo.de>
2022-02-14 10:26:16 +0100

    add readme + update deployment script

--- wordpress-volumes/deploy.sh
+++ wordpress-volumes/deploy.sh
@@ -4,3 +4,5 @@ kubectl create -f pv-claim.yml
 kubectl create -f wordpress-secrets.yml
 kubectl create -f wordpress-db.yml
 kubectl create -f wordpress-db-service.yml 
+kubectl create -f wordpress-web.yml
+kubectl create -f wordpress-web-service.yml
